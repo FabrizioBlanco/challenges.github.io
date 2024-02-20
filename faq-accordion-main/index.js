3df0c5d7f67ad4c62bc8f7a7d7c882df464c9e9d
FabrizioBlanco <Fabrizio.blanco2021@gmail.com>
2024-02-20 14:38:25 -0300

    Work in progress
    bug on icons in 375px resolution.

--- faq-accordion-main/index.js
+++ faq-accordion-main/index.js
@@ -1,24 +1,20 @@
 const plusList = document.querySelectorAll(".question__plus")
-plusList.forEach(plus => {
-    let active = false
-    plus.addEventListener("click", () => {
-        console.log("hola")
+let active=false
+for (const plus of plusList) {
+    plus.addEventListener("click",()=>{
         const answer = plus.nextElementSibling
-        try {
-            if (!active) {
-                answer.style.display = "flex"
-                active = true
-                plus.removeAttribute("src")
-                plus.setAttribute("src", "assets/images/icon-minus.svg")
-            } else {
-                answer.style.display = "none"
-                active = false
-                plus.removeAttribute("src")
-                plus.setAttribute("src", "assets/images/icon-plus.svg")
-            }
-        } catch (error) {
-            console.log(error)
+        if(!active){
+            answer.style.display= "flex"
+            active = true
+            plus.removeAttribute("src")
+            plus.setAttribute("src","assets/images/icon-minus.svg")
+            
+        }else{
+            answer.style.display="none"
+            active = false
+            plus.removeAttribute("src")
+            plus.setAttribute("src","assets/images/icon-plus.svg")
+
         }
     })
-})
-
+}
